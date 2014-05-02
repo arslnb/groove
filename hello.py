b95e45e0628609b88dcb8224e87faacdee6bc7d6
Arsalan Bashir <mail@arsalanbashir.com>
2014-05-02 15:13:17 +0530

    Fixed output and basic file rendering

--- hello.py
+++ hello.py
@@ -1,13 +1,27 @@
 import sys
+import os
 from flask import Flask
 app = Flask(__name__)
 
+if sys.platform == "darwin":
+
+	local = os.path.join(os.path.expanduser('~'), 'Groove/Local')
+	shared = os.path.join(os.path.expanduser('~'), 'Groove/Shared')
+
+	if not os.path.isdir(local) and not os.path.isdir(shared):
+		os.makedirs(local)
+		os.makedirs(shared)
+		print "OS X Directory"
+	else: 
+		print "Welcome Back!"
+
+elif sys.platform == "win32":
+	print "Windows Directory"
+	print "I'll add support for this soon."
+
 @app.route("/")
 def hello():
-    if sys.platform == "darwin":
-    	return "Nice Mac"
-    elif sys.platform == "cygwin":
-    	return "Fuckin windows"
+	return "success"
 
 if __name__ == "__main__":
     app.run()
--- run.py
+++ run.py
@@ -11,14 +11,43 @@ logo = """
 
 import click
 import time
+import sys
 import os
 from colorama import init, Fore, Back, Style
 init()
+
+if sys.platform == "darwin":
+	
+	local = os.path.join(os.path.expanduser('~'), 'Groove/Local')
+	shared = os.path.join(os.path.expanduser('~'), 'Groove/Shared')
+
+	if not os.path.isdir(local) and not os.path.isdir(shared):
+		os.makedirs(local)
+		os.makedirs(shared)
+		print "OS X Directory"
+	else: 
+		print "Welcome Back!"
+
+elif sys.platform == "win32":
+	print "Windows Directory"
+	print "I'll add support for this soon."
 
 
 @click.command()
 def hello():
 	print(Fore.WHITE + logo)
+
+	holder = []
+
+	for file in os.listdir(os.path.join(os.path.expanduser('~'), 'Groove/Local')):
+		if file.endswith(".mp3"):
+			print(Fore.CYAN + file)
+			holder.append(file)
+
+	value = click.prompt('Lets get this arty started, or what?', type=int)
+
+	print(Fore.RED + holder[value - 1])
+	print "Playing..."
 
 if __name__ == '__main__':
 	hello()
